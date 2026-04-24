8fccbe65792da39649b44f02976b8651239918f3
mmartic93 <maria.marti@icn2.cat>
2026-04-24 16:53:55 +0200

    height 0.5

--- data_processing/preview_service.py
+++ data_processing/preview_service.py
@@ -106,7 +106,7 @@ def get_signal_peaks(y_raw: np.ndarray):
     # 2. Unified Parameters (Sigma-based thresholding + Minimum distance)
     # distance=300 helps avoid multiple detections in noisy wave cycles
     params = {
-        'height': std_val * 0.2,
+        'height': std_val * 0.5,
         'prominence': std_val * 1.2,
         'distance': 300
     }
