213039fa0c6d0e8bb9899824397ee3700305a505
RPT21 <marremiau@outlook.com>
2026-09-07 16:15:36 +0200

    LoadData Optimization and ImportantFunctions deletion.
    Now LoadData is more flexible, and permits the DAQ to have different signal names. Before it only accepted Voltage, now it only checks if the important columns are present (Time, IsMoving_Bool, Motor_Up_Down_Bool).
    
    ImportantFunctions.py has been deleted, and only the useful functions have been preserved. They have been moved inside the LoadData.py

--- data_processing/LoadData.py
+++ data_processing/LoadData.py
@@ -1,33 +1,7 @@
 import os
 import re
-import logging
-
 import numpy as np
 import pandas as pd
-from data_processing.ImportantFunctions import merge_DAQ_data, synchronize_dataframes
-
-
-# %% --------------------------------------------------------------------------
-# INITIAL CONFIGURATION
-# -----------------------------------------------------------------------------
-
-logging.basicConfig(level=logging.ERROR,
-                    format="%(levelname)s: %(message)s"
-)
-
-REQ_LEVEL = 25
-logging.addLevelName(REQ_LEVEL, "REQ")
-
-def req(self, message, *args, **kwargs):
-    if self.isEnabledFor(REQ_LEVEL):
-        self._log(REQ_LEVEL, message, args, **kwargs)
-
-logging.Logger.req = req
-logger0 = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    logger0.setLevel(logging.INFO)
-
 
 # %% --------------------------------------------------------------------------
 # PARSE CUSTOM TIME TO SECONDS
@@ -54,7 +28,6 @@ def LTIME_to_seconds(LTIME):
     
     return total_time
 
-
 # %% --------------------------------------------------------------------------
 # RENAME RAWDATA DICTIONARIES
 # -----------------------------------------------------------------------------
@@ -64,17 +37,189 @@ MotColumnsRenames = {
     'MC SW Overview - Actual Position(mm)': 'Position',
     'MC SW Force Control - Measured Force(N)': 'Force',
     'MC SW Force Control - Target Force(N)': 'TargetForce',
-    'LINMOT_MOVING_BOOL': 'Bool1',
-    'LINMOT_UP_AND_DOWN_BOOL': 'Bool2'
+    'LINMOT_MOVING_BOOL': 'IsMoving_Bool',
+    'LINMOT_UP_AND_DOWN_BOOL': 'Motor_Up_Down_Bool'
 }
 
 DaqColumnsRenames = {
     'Time (s)': 'Time',
-    'Voltage': 'Voltage',
-    'LinMot_Enable': 'Bool1',
-    'LinMot_Up_Down': 'Bool2'
+    'LinMot_Enable': 'IsMoving_Bool',
+    'LinMot_Up_Down': 'Motor_Up_Down_Bool'
 }
 
+# %% --------------------------------------------------------------------------
+# VALIDATE BINARY COLUMN FUNCTION
+# -----------------------------------------------------------------------------
+
+def validate_binary_column(df, column):
+    # Check that all te values are {0, 1}
+    if not df[column].isin([0, 1]).all():
+        # Identify which are the values that are not 0 or 1
+        wrong_values = df[~df[column].isin([0, 1])][column].unique()
+        raise ValueError(f"Error: The column '{column}' contains not binary values: {wrong_values}")
+
+
+# %% --------------------------------------------------------------------------
+# SYNCHRONIZE DATAFRAMES FUNCTION
+# -----------------------------------------------------------------------------
+
+def synchronize_dataframes(dataframes_list, time_col='Time (s)', filter_time=True, binary_cols=("LinMot_Enable", "LinMot_Up_Down")):
+    """
+    Synchronizes a list of DataFrames to the highest sampling rate found among them.
+    It does a temporal boundary alignment as well (make all data have the same physical duration).
+    IMPORTANT: All dataframes have to start at the same timestep (for example, time = 0) and have different columns
+
+    Args:
+        dataframes_list (list): List of Pandas DataFrames.
+        time_col (str): The name of the time column (must be present in all DFs).
+
+    Returns:
+        List of Pandas DataFrames having the same time column as an index
+    """
+
+    if not dataframes_list:
+        return []
+
+    # =================================================================
+    # STEP 0: Temporal Boundary Alignment: all data represent the same physical duration of the experiment
+    # =================================================================
+
+    # Check that timestamps are correct:
+    for df in dataframes_list:
+        if not (df[time_col].is_monotonic_increasing and df[time_col].is_unique):
+            raise ValueError("The time column must be strictly increasing for interpolation.")
+
+    # Check that all dataframes start at the same time
+    valor_ref = dataframes_list[0].iloc[0][time_col]
+    all_equal = all(df.iloc[0][time_col] == valor_ref for df in dataframes_list)
+    if not all_equal:
+        raise Exception("The dataframes don't start at the same time")
+
+    if filter_time:
+        # Find the elapsed time for each dataframe and select the smallest one
+        print("Analysing time duration...")
+        min_end_time = float('inf')
+        for i, df in enumerate(dataframes_list):
+            current_time = df[time_col].iloc[-1]
+            print(f"  - DataFrame {i}: Time duration = {current_time:.6f} s")
+            if current_time < min_end_time:
+                min_end_time = current_time
+        print(f"The experiment ended at time: {min_end_time:.4f} seconds.")
+
+        # Filter the dataframes to make them have the same time duration
+        for i in range(len(dataframes_list)):
+            # Filter the dataframes to the min_end_time timestamp (ignore old index [drop=True] and create a new one)
+            dataframes_list[i] =\
+                dataframes_list[i][dataframes_list[i][time_col] <= min_end_time].copy().reset_index(drop=True)
+        print("All dataframes now have the same time duration")
+
+    # =================================================================
+    # STEP 1: Find the Highest Sampling Rate DataFrame
+    # =================================================================
+    min_time_step = float('inf')
+    master_time_index = None
+
+    print("Analyzing sampling rates...")
+
+    for i, df in enumerate(dataframes_list):
+
+        current_step = df[time_col].diff().mean()
+        print(f"  - DataFrame {i}: Average timestep = {current_step:.6f} s")
+
+        if current_step < min_time_step:
+            # Save the timestamp of the highest sampling rate DataFrame
+            master_time_index = df[time_col].values
+            min_time_step = current_step
+
+    print(f"Target sampling step: {min_time_step:.6f} s")
+
+    # =================================================================
+    # STEP 2: Resample and Interpolate all DataFrames
+    # =================================================================
+    synced_dataframes = []
+
+    for df in dataframes_list:
+
+        # 1. Prepare the DF: remove duplicates and set the time column as the index
+        df_temp = df.drop_duplicates(subset=[time_col]).set_index(time_col)
+
+        # 2. Reindex and Interpolate
+        # - union(): Merges the dataframe's timestamp with the higher sampling rate timestamp
+        # - interpolate(method='index'): Uses the actual time values to calculate data in the new timesteps
+        # - loc[master_time_index]: Keeps only the points belonging to the master timestamp
+        df_sync = (
+            df_temp
+            .reindex(df_temp.index.union(master_time_index))
+            .interpolate(method='index')
+            .loc[master_time_index]
+        )
+
+        # 3. Ensure binary columns to be int data type
+        found_binary_columns = [col for col in binary_cols if col in df_sync.columns]
+        if found_binary_columns:
+            # Apply rounding and cast to integer for all matching columns simultaneously (Vectorization)
+            df_sync[found_binary_columns] = df_sync[found_binary_columns].round().astype(int)
+
+        synced_dataframes.append(df_sync)
+
+    return synced_dataframes
+
+# %% --------------------------------------------------------------------------
+# MERGE DAQ RAWDATA FROM DIFFERENT TASKS INTO A SINGLE DATAFRAME
+# -----------------------------------------------------------------------------
+
+def merge_DAQ_data(folder_path, time_col='Time (s)'):
+    files = [str(f) for f in os.listdir(folder_path) if f.endswith('.pkl')]
+
+    dataframes = []
+
+    for file in files:
+        try:
+            df = pd.read_pickle(os.path.join(folder_path, file))
+            dataframes.append(df)
+        except Exception as e:
+            raise Exception(f'Error reading DAQ file {file}: {e}.')
+
+    # Synchronize dataframes
+    synced_dataframes = synchronize_dataframes(dataframes, time_col=time_col)
+
+    # Concatenate directly (since all DFs share the same master time index)
+    df = pd.concat(synced_dataframes, axis=1)
+
+    # Restore the time index as a standard column
+    df.index.name = time_col
+    df = df.reset_index()
+
+    # Check synchronization columns:
+    if not ("LinMot_Enable" in df.columns and "LinMot_Up_Down" in df.columns):
+        raise Exception("Error, LinMot_Up_Down and LinMot_Enable are not present in the dataframes")
+
+    # Validate that they have valid data
+    for col in df.columns:
+        if col in ["LinMot_Enable", "LinMot_Up_Down"]:
+            validate_binary_column(df, col)
+
+    # Calculate the differences between adjacent values
+    diff = df["LinMot_Enable"].diff()
+
+    # Search indices
+    up_index = diff.index[diff == 1].tolist()
+    down_index = diff.index[diff == -1].tolist()
+
+    if len(up_index) != 1 and len(down_index) != 1:
+        raise Exception("Error, LinMot_Enable start and end position not found")
+    else:
+        up_index = up_index[0] - 1
+        down_index = down_index[0]
+
+    print("Found rising edge and falling edge positions in LinMot_Enable")
+    print(f"Rising edge (from 0 to 1): {up_index}")
+    print(f"Falling edge (from 1 to 0): {down_index}")
+
+    # Filter dataframes using the calculated indices and take the first row as the time reference value
+    df_final = df.loc[up_index : down_index].reset_index(drop=True)
+    df_final[time_col] -= df_final[time_col].iloc[0]
+    return df_final
 
 # %% --------------------------------------------------------------------------
 # LOAD MOTOR RAWDATA
@@ -98,10 +243,10 @@ def LoadMotorFile(ExpPath):
     # Find files with CSV extension
     files = [f for f in os.listdir(ExpPath) if f.endswith('.csv')]
     if len(files) == 0:
-        logger0.error(f'No motor data file found in {ExpPath}.')
+        print(f'No motor data file found in {ExpPath}.')
         return None
     elif len(files) > 1:
-        logger0.error(f'More than one motor data file found in {ExpPath}, the program cannot proceed.')
+        print(f'More than one motor data file found in {ExpPath}, the program cannot proceed.')
         return None
 
     # Define the MotorFile path
@@ -112,17 +257,13 @@ def LoadMotorFile(ExpPath):
         dfMot = pd.read_csv(MotorFile, header=0, index_col=False,
                             delimiter=',', decimal='.')
     except Exception as e:
-        logger0.error(f'Error reading Motor file {MotorFile}: {e}.')
+        print(f'Error reading Motor file {MotorFile}: {e}.')
         return None
     
-    # Drop non-defined columns
-    dropcols = [col for col in dfMot.columns if col not in MotColumnsRenames]
-    dfMot = dfMot.drop(columns=dropcols)
-    
-    # Check if all defined columns exist
+    # Check if required source columns exists, and then rename them
     for col in MotColumnsRenames.keys():
         if col not in dfMot.columns:
-            logger0.error(f'Column {col} not found in {MotorFile}.')
+            print(f'Column {col} not found in {MotorFile}.')
             return None
     
     # Rename defined columns
@@ -133,8 +274,8 @@ def LoadMotorFile(ExpPath):
         'Position': float,
         'Force': float,
         'TargetForce': float,
-        'Bool1': int,
-        'Bool2': int
+        'IsMoving_Bool': int,
+        'Motor_Up_Down_Bool': int
     })
     
     # Corrections Time
@@ -143,13 +284,12 @@ def LoadMotorFile(ExpPath):
     
     # Corrections Position
     dfMot['Position'] -= dfMot['Position'].min()
-    
+
     # Corrections Force and TargetForce
-    dfMot['Force'] = - dfMot['Force']
-    dfMot['TargetForce'] = - dfMot['TargetForce']
+    dfMot['Force'] = -dfMot['Force']
+    dfMot['TargetForce'] = -dfMot['TargetForce']
     
     return dfMot
-
 
 # %% --------------------------------------------------------------------------
 # LOAD DAQ RAWDATA
@@ -173,15 +313,11 @@ def LoadDAQData(ExpPath):
     # Read the DAQ Data
     dfDaq = merge_DAQ_data(ExpPath)
     
-    # Drop non-defined columns:
-    dropcols = [col for col in dfDaq.columns if col not in DaqColumnsRenames]
-    dfDaq = dfDaq.drop(columns=dropcols)
-    
-    # Check if all defined columns exist
+    # Check if required source columns exists, and then rename them
     for col in DaqColumnsRenames.keys():
         if col not in dfDaq.columns:
-            logger0.error(f'Column {col} not found in the experiment folder {ExpPath}.')
-            return
+            print(f'Column {col} not found in the experiment folder {ExpPath}.')
+            return None
     
     # Rename defined columns
     dfDaq = dfDaq.rename(columns=DaqColumnsRenames)
@@ -189,15 +325,10 @@ def LoadDAQData(ExpPath):
     # Ensure columns have the correct data types
     dfDaq = dfDaq.astype({
         'Time': float,
-        'Voltage': float,
-        'Bool1': int,
-        'Bool2': int
+        'IsMoving_Bool': int,
+        'Motor_Up_Down_Bool': int
     })
 
-    # Corrections Voltage (find the starting point of motor movement and reference voltage from this point)
-    # j = dfDaq['Bool1'].eq(1).idxmax()
-    # dfDaq['Voltage'] -= dfDaq['Voltage'][:j].mean()
-    
     return dfDaq
 
 
@@ -211,15 +342,14 @@ def FindCycles(df):
     
     Parameters
     ----------
-    dataframe : Pandas DataFrame
-        Dataframe with two binary synchronization columns (Enable/Disable, Move_Up/Move_Down)
+    dataframe : Pandas Dataframe with two binary synchronization columns (Enable/Disable, Move_Up/Move_Down)
     
     Returns
     -------
     list of [start, end]
         List of cycles represented by their star and end indices.
     '''
-    state_series = df['Bool1'] + df['Bool2']
+    state_series = df['IsMoving_Bool'] + df['Motor_Up_Down_Bool']
     cycles = []
     prev_state = state_series.iloc[0]
     start = None
@@ -271,16 +401,16 @@ def ExtractCycles(ExpPath):
     
     # Motor sampling rate
     MotFs = 1 / dfMot['Time'].diff().mean()
-    logger0.info(f'Motor sampling rate: {MotFs}.')
+    print(f'Motor sampling rate: {MotFs}.')
     
     # DAQ sampling rate
     DaqFs = 1 / dfDaq['Time'].diff().mean()
-    logger0.info(f'DAQ sampling rate: {DaqFs}.')
+    print(f'DAQ sampling rate: {DaqFs}.')
 
     ### Synchronize motor and DAQ data ###
 
     # Calculate the differences between adjacent values
-    diff = dfMot['Bool1'].diff()
+    diff = dfMot['IsMoving_Bool'].diff()
 
     # Search indices
     up_index = diff.index[diff == 1].tolist()
@@ -313,22 +443,18 @@ def ExtractCycles(ExpPath):
     [dfDaq, dfMot] = synchronize_dataframes([dfDaq, dfMot],
                                             time_col='Time',
                                             filter_time=False,
-                                            binary_cols=['Bool1', 'Bool2'])
+                                            binary_cols=['IsMoving_Bool', 'Motor_Up_Down_Bool'])
 
     # Restore the time index as a standard column
     for df in [dfMot, dfDaq]:
         df.index.name = 'Time'
         df.reset_index(inplace=True)
 
-    # Estimate Velocity and Acceleration
-    dfMot['Velocity'] = (dfMot['Position'].diff()/1000) / dfMot['Time'].diff()
-    dfMot['Acceleration'] = dfMot['Velocity'].diff() / dfMot['Time'].diff()
-                    
     # Finding cycles
     MotCycles = FindCycles(dfMot)
     DaqCycles = FindCycles(dfDaq)
     if len(MotCycles) != len(DaqCycles):
-        logger0.info(f'Different number of cycles: Motor={len(MotCycles)}, DAQ={len(DaqCycles)}. Using minimum.')
+        print(f'Different number of cycles: Motor={len(MotCycles)}, DAQ={len(DaqCycles)}. Using minimum.')
     nCycles = min(len(MotCycles), len(DaqCycles))
     
     Cycles = []
@@ -339,7 +465,7 @@ def ExtractCycles(ExpPath):
         dfcyD = dfDaq.iloc[DaqCycles[idx][0] : DaqCycles[idx][1] + 1].reset_index(drop=True)
 
         # Find the first index where (State == 1) in the cycle interval for the Motor
-        mask = dfcyM['Bool2'].eq(1)
+        mask = dfcyM['Motor_Up_Down_Bool'].eq(1)
         if mask.any():
             i = mask.idxmax()
         else:
@@ -347,7 +473,7 @@ def ExtractCycles(ExpPath):
             continue
 
         # Find the first index where (State == 1) in the cycle interval for the DAQ
-        mask = dfcyD['Bool2'].eq(1)
+        mask = dfcyD['Motor_Up_Down_Bool'].eq(1)
         if mask.any():
             j = mask.idxmax()
         else:
